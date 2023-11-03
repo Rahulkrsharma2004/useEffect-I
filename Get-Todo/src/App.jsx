3cf32a853feb50542195886fad22884b3000c857
RahulKrSharma2004 <kumarrahulbasdiha@gmail.com>
2023-11-03 09:42:43 +0530

    added a new get-Todo app

---
+++ Get-Todo/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react"
+import PostItem from "./components/PostItem"
+import axios from "axios"
+
+const getData = async (url) => {
+  try {
+    let res = await axios.get(url)
+    const totalCount = +res.headers.get("x-total-count")
+
+    const totalPages = Math.ceil(totalCount / 10) // count/limit
+    const data = res.data
+
+    return {
+      data: data,
+      totalPages: totalPages,
+    }
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+function App() {
+  const [loading, setLoading] = useState(false)
+  const [posts, setPosts] = useState([])
+  const [err, setErr] = useState(false)
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
+
+  useEffect(() => {
+    fetchAndUpdateData(page)
+  }, [page])
+
+  const fetchAndUpdateData = async (page) => {
+    setLoading(true)
+    try {
+      let output = await getData(
+        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+      )
+      const { data, totalPages } = output
+      setTotalPages(totalPages)
+      setPosts(data)
+      setLoading(false)
+    } catch (error) {
+      setErr(true)
+      setLoading(false)
+    }
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (err) {
+    return <h1>Something went wrong.. please refresh</h1>
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+          PREVIOUS
+        </button>
+        <p>{page}</p>
+        <button
+          disabled={page === totalPages}
+          onClick={() => setPage(page + 1)}
+        >
+          NEXT
+        </button>
+      </div>
+      {posts.map((post) => (
+        <PostItem key={post.id} id={post.id} title={post.title} />
+      ))}
+    </>
+  )
+}
+
+export default App
---
+++ Get-Todo/src/components/PostItem.jsx
@@ -0,0 +1,10 @@
+const PostItem = ({ id, title }) => {
+    return (
+      <div>
+        <p>{id}</p>
+        <p>{title}</p>
+      </div>
+    )
+  }
+  
+  export default PostItem
